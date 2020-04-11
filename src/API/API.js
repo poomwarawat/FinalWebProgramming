bc522aef29de75c1a12d3b7c660329a9a79cb490
anuwatavis <anuwat.avis@gmail.com>
2020-04-11 16:09:07 +0700

    add mysql google cloud

--- src/API/API.js
+++ src/API/API.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default axios.create({
-    baseURL : "http://localhost:8888/runrena"
-})
+  baseURL: "http://localhost:8080/",
+});
 //http://localhost:8888/runrena
 //https://bonelab.000webhostapp.com/
---
+++ src/API/config/mySQL.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: "*****",
+  user: "*****",
+  password: "*****",
+  database: "*****",
+});
+
+module.exports = con;
