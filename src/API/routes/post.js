f05e4f4fce6c889f9d195138b3be15709beb5914
anuwatavis <anuwat.avis@gmail.com>
2020-04-12 10:57:59 +0700

    add post router

--- src/API/routes/post.js
+++ src/API/routes/post.js
@@ -9,6 +9,11 @@ router.get("/posts", (req, res) => {});
 //Create a new post
 router.post("/posts", (req, res) => {});
 
-//Delete post
+//Delete post and its comments from database
+
+router.delete("/post/:id", (req, res) => {});
+
+//Like post --update like in post
+router.put("/post/:id", (req, res) => {});
 
 module.exports = router;
