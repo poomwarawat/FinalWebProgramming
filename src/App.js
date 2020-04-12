24eb8a1d38d6467964db13f13f279d98bb056dee
anuwatavis <anuwat.avis@gmail.com>
2020-04-12 15:44:38 +0700

    add event page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom'
-import Start from './page/Start'
-import Register from './page/Register'
-import About from './page/About'
-import Contact from './page/Contact'
-import Profile from './page/Profile'
-import Graph from './page/Graph'
-import Setting from './page/Setting'
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Start from "./page/Start";
+import Register from "./page/Register";
+import About from "./page/About";
+import Contact from "./page/Contact";
+import Profile from "./page/Profile";
+import Graph from "./page/Graph";
+import Setting from "./page/Setting";
+import Event from "./page/Event";
 
 function App() {
   return (
     <div className="App">
-        <Switch>
-          <Route path='/' component={Start} exact></Route>
-          <Route path='/register' component={Register}></Route>
-          <Route path='/about' component={About}></Route>
-          <Route path='/contact' component={Contact}></Route>
-          <Route path='/profile' component={Profile}></Route>
-          <Route path='/graph' component={Graph}></Route>
-          <Route path='/setting' component={Setting}></Route>
-        </Switch>
+      <Switch>
+        <Route path="/" component={Start} exact></Route>
+        <Route path="/register" component={Register}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/contact" component={Contact}></Route>
+        <Route path="/profile" component={Profile}></Route>
+        <Route path="/graph" component={Graph}></Route>
+        <Route path="/setting" component={Setting}></Route>
+        <Route path="/event" component={Event}></Route>
+      </Switch>
     </div>
   );
 }
