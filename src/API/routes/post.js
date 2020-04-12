39438de70216b3b94e634563077fe20b39140455
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-04-12 15:21:09 +0700

    Add post with distance

--- src/API/routes/post.js
+++ src/API/routes/post.js
@@ -8,8 +8,8 @@ router.get("/posts", (req, res) => {});
 
 //Create a new post
 router.post("/posts", (req, res) => {
-    const sql = `INSERT INTO post (activity_title, userId) VALUES (?, ?)`;
-    const Data = [req.body.data, req.body.userId ]
+    const sql = `INSERT INTO post (activity_title, userId, total_distance, pace_average, description) VALUES (?, ?, ?, ?, ?)`;
+    const Data = [req.body.data, req.body.userId, req.body.totalDistance, req.body.paceAverage, req.body.description ]
     const value = Object.values(Data)
     con.query(sql, value, (err, result) =>{
         if(err) throw err
--- src/component/PostStatus.js
+++ src/component/PostStatus.js
@@ -1,64 +1,134 @@
-import React, { Component } from 'react'
-import API from '../API/API'
-import UploadPost from '../component/UploadPost'
-
+import React, { Component } from "react";
+import API from "../API/API";
+import UploadPost from "../component/UploadPost";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardBody,
+  Button,
+  CustomInput,
+  CardHeader,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
 export default class PostStatus extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            data : '',
-            date : new Date(),
-            userId : ''
-        }
-    }
-    componentWillMount(){
-        const Key = new FormData()
-        Key.append('token', localStorage.getItem('key') )
-        API.post("/auth-token", Key)
-        .then(res => {
-            if(res.data.userId){
-                this.setState({
-                    userId : res.data.userId
-                })
-            }
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        data : '',
+        date : new Date(),
+        userId : '',
+        description : '',
+        totalDistance : '',
+        paceAverage : ''
 
-    handleChange = (e) =>{
-        const name = e.target.id
-        const value = e.target.value
-        this.setState({
-            [name] : value
-        })
-    }
-    handleClick = () =>{
-        const Data = new FormData()
-        Data.append('data', this.state.data)
-        Data.append('date', this.state.date)
-        Data.append('userId', this.state.userId)
-        API.post("/post/posts", Data)
-        .then(res => {
-            if(res.data.post == true){
-                alert("Post complete")
-            }
-        })
-    }
-    render() {
-        return (
-            <div>
-                <div className="mt-4">
-                    <h3>Your Status</h3>
-                    <div className="mt-2 post-box">
-                        <div className="head-post-box">
-                            <p>Update your today running status</p>
-                        </div>
-                        <textarea placeholder="   Enter your status..." value={this.state.data} id="data" onChange={this.handleChange}></textarea>
-                        <div className="head-post-box">
-                            <button className="btn btn-info w-100" onClick={this.handleClick}>POST</button>
-                        </div>
-                    </div>
-                </div>
+    };
+  }
+  componentWillMount(){
+    const Key = new FormData()
+    Key.append('token', localStorage.getItem('key') )
+    API.post("/auth-token", Key)
+    .then(res => {
+        if(res.data.userId){
+            this.setState({
+                userId : res.data.userId
+            })
+        }
+    })
+}
+  handleChange = (e) => {
+    const name = e.target.id;
+    const value = e.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+  handleClick = () =>{
+    const Data = new FormData()
+    console.log(this.state)
+    Data.append('data', this.state.data)
+    Data.append('date', this.state.date)
+    Data.append('userId', this.state.userId)
+    Data.append('description', this.state.description)
+    Data.append('totalDistance', this.state.totalDistance)
+    Data.append('paceAverage', this.state.paceAverage)
+    console.log(this.state)
+    API.post("/post/posts", Data)
+    .then(res => {
+        if(res.data.post == true){
+            alert("Post complete")
+        }
+    })
+}
+  render() {
+    return (
+      <div>
+        <div className="mt-4 container">
+          <Card>
+            <CardHeader id="test">Post</CardHeader>
+            {/* <CardTitle>Update your today running status</CardTitle> */}
+            <Form>
+              <Row form className="ml-5 mr-5 mt-4">
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="titleActivity">Title Activity</Label>
+                    <Input
+                      onChange={this.handleChange}
+                      type="text"
+                      name="data"
+                      id="data"
+                      placeholder="Bang Khun Running"
+                      required
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md={8}>
+                  <FormGroup>
+                    <Label for="description">Description</Label>
+                    <Input type="text" name="description" onChange={this.handleChange} id="description" placeholder="Beat Yesterday" required />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row form className="ml-5 mr-5">
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="totalDistance">Distance</Label>
+                    <Input type="number" name="totalDistance" onChange={this.handleChange} id="totalDistance" placeholder="30 KM" required />
+                  </FormGroup>
+                </Col>
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="totalTime">Time</Label>
+                    <Input type="number" name="totalTime" onChange={this.handleChange} id="totalTime" placeholder="15.00 Min" required />
+                  </FormGroup>
+                </Col>
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="paceAverage">Pace</Label>
+                    <Input type="number" name="paceAverage" onChange={this.handleChange} id="paceAverage" placeholder="4.05 Min/Km" required />
+                  </FormGroup>
+                </Col>
+              </Row>
+            </Form>
+          </Card>
+          <div className="head-post-box">
+            <button className="btn btn-info w-100" onClick={this.handleClick}>
+              POST
+            </button>
+          </div>
+        </div>
+        {/* {this.state.post.reverse().map((datas) => {
+          return (
+            <div key={datas.id}>
+              <UploadPost data={datas}></UploadPost>
             </div>
-        )
-    }
+          );
+        })} */}
+      </div>
+    );
+  }
 }
