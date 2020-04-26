5363494bae13b0e33528b113cc7b536c9aa77711
anuwatavis <anuwat.avis@gmail.com>
2020-04-26 20:06:34 +0700

    add chart

--- src/page/Graph.js
+++ src/page/Graph.js
@@ -3,80 +3,41 @@ import Nevigator from "../component/Nevigator";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 import { Card, Row, Col, CardHeader, CardBody, CardTitle } from "reactstrap";
 import { Bar } from "react-chartjs-2";
-
+import MonthReport from "../component/Graph/MonthReport";
+import API from "../API/API";
 export default class Explorer extends Component {
+  state = {
+    userId: null,
+  };
+  componentDidMount = () => {
+    const Key = new FormData();
+    Key.append("token", localStorage.getItem("key"));
+    API.post("/auth-token", Key).then((res) => {
+      if (res.data.userId) {
+        this.setState({
+          userId: res.data.userId,
+        });
+      }
+    });
+  };
   render() {
-    const data = {
-      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-      datasets: [
-        {
-          label: "Beat",
-          backgroundColor: [
-            "#4d4c7d",
-            "#fb7b6b",
-            "#d7385e",
-            "#4d4c7d",
-            "#fb7b6b",
-            "#d7385e",
-            "#4d4c7d",
-            "#fb7b6b",
-            "#d7385e",
-          ],
-          highlightFill: "yellow",
-          borderColor: "white",
-          borderWidth: 2,
-          barPercentage: 0.4,
-          data: ["20", "30", "40", "20", "30", "40", "20", "30", "40"],
-        },
-      ],
-    };
     return (
       <div>
         <Row>
           <Col md="3">
             <Nevigator />
           </Col>
-          <Col md="9">
+          <Col md="8">
             <Card>
               <CardHeader>Overview</CardHeader>
               <CardBody>
                 <Row>
                   <Col md="12">
                     <CardTitle>
-                      <div>Week Report</div>
+                      <div>Month Report</div>
                     </CardTitle>
                   </Col>
-                  <Col md="4">
-                    <Bar
-                      data={data}
-                      width={10}
-                      height={300}
-                      options={{
-                        maintainAspectRatio: false,
-                        scales: {
-                          yAxes: [
-                            {
-                              ticks: {
-                                beginAtZero: true,
-                              },
-                            },
-                          ],
-                        },
-                        tooltips: {
-                          displayColors: false,
-                          titleFontSize: 16,
-                          bodyFontSize: 14,
-                          xPadding: 10,
-                          yPadding: 10,
-                          callbacks: {
-                            label: (tooltipItem, data) => {
-                              return `${tooltipItem.value} Km`;
-                            },
-                          },
-                        },
-                      }}
-                    />
-                  </Col>
+                  <Col md="12">{this.state.userId ? <MonthReport userId={this.state.userId} /> : null}</Col>
                 </Row>
               </CardBody>
             </Card>
