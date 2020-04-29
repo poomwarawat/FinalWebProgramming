db6e3ebf6f9ddefe9d4104d31001407b957452cd
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-04-30 02:49:43 +0700

    config photo like

--- src/component/Album/AlbumBox.js
+++ src/component/Album/AlbumBox.js
@@ -7,7 +7,7 @@ export default class AlbumBox extends Component {
             <div >
                 <div className="album-box mt-4 pt-2">
                     <div className="img-album">
-                        <Link to="/myphoto">
+                        <Link to={`/myphoto/${this.props.Data.userId}/${this.props.Data.albumname}`}>
                             <img src="http://www.thailandoutdoor.com/wp-content/uploads/12443704_10207970802445350_1538187090_o.jpg"/>
                         </Link>
                     </div>
