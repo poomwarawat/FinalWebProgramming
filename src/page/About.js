3a05da8144e9c9c4b8ddd515fdb7b3ff051d832d
anuwatavis <anuwat.avis@gmail.com>
2020-04-30 01:26:44 +0700

    add contace page and about

--- src/page/About.js
+++ src/page/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../component/Header";
 import PicSlide from "../component/PicSlide";
+
 export default class About extends Component {
   render() {
     return (
@@ -9,6 +10,11 @@ export default class About extends Component {
         <div className="container pt-4 detial">
           <h1>About Us</h1>
           <PicSlide></PicSlide>
+          <h5 className="text-center mt-4">
+            Runrena is an online society of runners. <br></br>Runners can actually participate in the running event through the website. <br></br>under the
+            concept of the founders and developers that "Run UNTIL BIB"
+          </h5>
+          <h6 className="text-center ">If health is important Therefore, running is important.</h6>
         </div>
       </div>
     );
